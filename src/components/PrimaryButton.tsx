8b0e6a33b7706290d42f19351ce82684c77f66aa
조인식 <imsif42@naver.com>
2025-01-01 21:12:09 +0900

    remove: 불필요파일

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -8,9 +8,9 @@ interface IPrimaryButtonProps {
 }
 
 const activeV1 = 'bg-[#1d4ed8] text-white rounded-lg h-12 text-md';
-const inActiveV1 = 'bg-buttonInActive text-main rounded-lg h-12 text-md';
+const inActiveV1 = 'bg-buttonInActive text-[#1d4ed8] rounded-lg h-12 text-md';
 const activeV2 = 'bg-[#1d4ed8] text-white rounded-full h-9 text-sm';
-const inActiveV2 = 'bg-buttonInActive text-main rounded-full h-9 text-sm';
+const inActiveV2 = 'bg-buttonInActive text-[#1d4ed8] rounded-full h-9 text-sm';
 
 const disabledStyle =
   'disabled:bg-main disabled:text-white disabled:opacity-70';
