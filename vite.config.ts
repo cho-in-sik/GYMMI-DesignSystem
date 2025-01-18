fe57211c525c3aaafe93bf57d5d1a0099b4cfa77
조인식 <imsif42@naver.com>
2025-01-19 01:14:10 +0900

    feat: 배포 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: './tsconfig.build.json',
+    }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/styles/index.css',
+          dest: '',
+        },
+      ],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.tsx'),
+      fileName: (format) => `index.${format}.js`,
+      name: 'gymmi-designSystem',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+});
